35c7f5a357d1d6b5b72f80de08628e447f13d695
William Reticena <william.wsd43@gmail.com>
2022-10-03 16:44:45 -0300

    criando formulário de fragilidade

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,13 @@
 <template>
-  <div></div>
+  <div>
+    <FragilityForm />
+  </div>
 </template>
 
 <script>
+  import FragilityForm from "@/components/evaluationsForms/FragilityForm.vue";
 
 export default {
+  components: {FragilityForm}
 }
 </script>
