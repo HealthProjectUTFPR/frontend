e0dd57db98088e24c62468d41bfc494507fe9ba1
celin07 <marcelodaniel@alunos.utfpr.edu.br>
2022-10-08 12:33:00 -0300

    feat: tela de cadastro Fixes #73

---
+++ pages/signup.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <SignupC />
+    </div>
+  </template>
+    
+  <script>
+  import SignupC from '@/components/signup/signupC.vue'
+  export default {
+      components: {
+         SignupC,
+      },
+    layout: 'empty',
+  }
+  </script>
+    
+  <style>
+  </style>
+    
