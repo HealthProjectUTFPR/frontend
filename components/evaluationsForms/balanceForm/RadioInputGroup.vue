1d87f9065f5054d139ae7bc5854e1fafc35d1c66
William Reticena <william.wsd43@gmail.com>
2022-10-27 17:12:10 -0300

    refactor: troca do input radio pelo slide

--- components/evaluationsForms/balanceForm/RadioInputGroup.vue
+++ components/evaluationsForms/balanceForm/RadioInputGroup.vue
@@ -1,33 +1,14 @@
 <template>
   <div class="mt-2">
-    <el-radio-group v-model="optionsGroups[group]" class="flex-important flex-col">
-      <el-radio :label="0">
-        0
-        <span v-show="screenWidthSize >= 768" class="custom-color-gray ml-24 inline-block font-normal">{{ getDescription(group, 0) }}</span>
-      </el-radio>
+    <span class="demonstration"></span>
+    <el-slider
+      v-model="optionsGroups[group]" max="4"
+      :step="1"
+      input-size="large"
+      input>
+    </el-slider>
 
-      <el-radio :label="1" class="md:mt-2">
-        1
-        <span v-show="screenWidthSize >= 768" class="custom-color-gray ml-24 inline-block font-normal">{{ getDescription(group, 1) }}</span>
-      </el-radio>
-
-      <el-radio :label="2" class="md:mt-2">
-        2
-        <span v-show="screenWidthSize >= 768" class="custom-color-gray ml-24 inline-block font-normal">{{ getDescription(group, 2) }}</span>
-      </el-radio>
-
-      <el-radio :label="3" class="md:mt-2">
-        3
-        <span v-show="screenWidthSize >= 768" class="custom-color-gray ml-24 inline-block font-normal">{{ getDescription(group, 3) }}</span>
-      </el-radio>
-
-      <el-radio :label="4" class="md:mt-2">
-        4
-        <span v-show="screenWidthSize >= 768" class="custom-color-gray ml-24 inline-block font-normal">{{ getDescription(group, 4) }}</span>
-      </el-radio>
-    </el-radio-group>
-
-    <span v-show="screenWidthSize < 768" class="mt-3 block text-xs md:mt-0 md:inline">{{ getDescription(group, optionsGroups[group]) }}</span>
+    <span class="mt-3 block text-xs md:text-sm md:mt-0 md:inline">{{ getDescription(group, optionsGroups[group]) }}</span>
   </div>
 </template>
 
