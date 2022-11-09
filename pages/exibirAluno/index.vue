a72474371dbec985f43df95458c32228b0b88b62
Pedro <pedro.bortolucci1.ph@gmail.com>
2022-11-09 20:21:19 -0300

    feat: botão de editar os dados

--- pages/exibirAluno/index.vue
+++ pages/exibirAluno/index.vue
@@ -11,7 +11,7 @@
     </div>
     <div class="mt-5 flex items-center justify-center gap-3">
       <div class="mb-2 flex flex-row">
-          <div><img src="/images/user.png" class=" rounded-full object-cover" height="70" width="70"/></div>
+          <div class="mr-2"><img src="/images/user.png" class=" rounded-full object-cover" height="70" width="70"/></div>
           <div class="mb-2 flex flex-col"><span><b>Nome:</b> Mario Andrade</span>
             <span><b><b></b>Idade:</b> 56</span></div>
       </div>
@@ -139,7 +139,79 @@
       
       <div class="flex justify-end pr-2 pb-2">
   
-          <el-button type="primary" icon="el-icon-edit-outline" circle size="medium" ></el-button>
+          
+          <el-button type="primary" icon="el-icon-edit-outline" circle size="medium" @click="toggleModalPassword = true, modalPassword.inputOldPassword = '', modalPassword.inputNewPassword = ''"></el-button>
+          <div v-if="toggleModalPassword" class="absolute inset-0 z-50 flex items-center justify-center overflow-y-auto overflow-x-hidden">
+            <div class=" relative m-auto">
+              <div class=" h-auto pb-3 w-80 rounded bg-white">
+                <div class=" mt-5 text-center">
+                  <span class=" text-base font-bold">Editar Dados</span>
+                </div>
+                <el-form ref="modalPasswordForm" :model="modalPassword" :rules="rulesModalPassword">
+                  <div class=" mx-3 mt-3 mb-2">
+                    <el-form-item prop="birthDate">
+                      <el-input v-model="modalPassword.birthDate" placeholder="Data de nascimento"></el-input>
+                    </el-form-item>
+                  </div>
+                  <div class=" mx-3 mt-3 mb-2">
+                    <el-form-item prop="address">
+                      <el-input v-model="modalPassword.address" placeholder="Endereço"></el-input>
+                    </el-form-item>
+                  </div>
+                  <div class="mx-3">
+                    <el-form-item prop="contact">
+                      <el-input v-model="modalPassword.contact" placeholder="Contato"></el-input>
+                    </el-form-item>
+                  </div>
+                  <div class="mx-3">
+                    <el-form-item prop="emergency contact">
+                      <el-input v-model="modalPassword.emergencyContact" placeholder="Contato de emergência"></el-input>
+                    </el-form-item>
+                  </div>
+                  <div class="mx-3">
+                    <el-form-item prop="weight">
+                      <el-input v-model="modalPassword.weight" placeholder="Peso"></el-input>
+                    </el-form-item>
+                  </div>
+                  <div class="mx-3">
+                    <el-form-item prop="stature">
+                      <el-input v-model="modalPassword.stature" placeholder="Estatura"></el-input>
+                    </el-form-item>
+                  </div>
+                  <div class="mx-3">
+                    <el-form-item prop="ethnicity">
+                      <el-input v-model="modalPassword.ethnicity" placeholder="Raça"></el-input>
+                    </el-form-item>
+                  </div>
+                  <div class="mx-3">
+                    <el-form-item prop="gender">
+                      <el-input v-model="modalPassword.gender" placeholder="Sexo"></el-input>
+                    </el-form-item>
+                  </div>
+                  <div class="mx-3">
+                    <el-form-item prop="imc">
+                      <el-input v-model="modalPassword.imc" placeholder="IMC"></el-input>
+                    </el-form-item>
+                  </div>
+                  <div class="mx-3">
+                    <el-form-item prop="healthPlan">
+                      <el-input v-model="modalPassword.healthPlan" placeholder="Plano de saúde"></el-input>
+                    </el-form-item>
+                  </div>
+                  <div class="mx-3">
+                    <el-form-item prop="observation">
+                      <el-input v-model="modalPassword.observation" placeholder="Observações"></el-input>
+                    </el-form-item>
+                  </div>
+                </el-form>
+                  <div class="mx-3 mt-11 flex justify-between">
+                  <el-button type="danger" @click="toggleModalPassword = false">Cancelar</el-button>
+                  <el-button type="success" @click="toggleModalPassword = false, submitPassword()">Confirmar</el-button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div v-if="toggleModalPassword" class=" absolute inset-0 z-40 bg-black opacity-25"></div>
 
       </div>
               
@@ -147,89 +219,10 @@
       
 
       
-       <!-- <div class=" mb-10 h-5 w-full bg-gray-700">
-        <div class="text-center">
-          <span class=" font-sans text-xs font-bold text-white">E-mail</span>
-        </div>
-        <div class=" mr-3 flex items-center justify-between">
-          <span class=" my-1 ml-3 block font-sans text-xs font-medium leading-5"></span>
-          <el-button type="primary" icon="el-icon-edit-outline" circle size="mini" ></el-button>
-          <div  class="absolute inset-0 z-50 flex items-center justify-center overflow-y-auto overflow-x-hidden">
-            <div class=" relative m-auto">
-              <div class=" h-72 w-80 rounded bg-white">
-                <div class=" mt-5 text-center">
-                  <span class=" text-base font-bold">Editar Email</span>
-                </div>
-                <el-form ref="modalEmailForm" >
-                  <div class=" mx-3 mt-3 mb-2">
-                    <el-form-item prop="inputNewEmail">
-                      <el-input type="email" placeholder="Novo Email"></el-input>
-                    </el-form-item>
-                  </div>
-                  <div class=" mx-3 mb-2">
-                    <el-form-item prop="inputConfirmEmail">
-                      <el-input  type="email" placeholder="Confirmar E-mail"></el-input>
-                    </el-form-item>
-                  </div>
-                  <div class="mx-3">
-                    <el-form-item prop="inputOldPassword">
-                      <el-input  placeholder="Senha Atual" show-password></el-input>
-                    </el-form-item>
-                  </div>
-                </el-form>
-                <div class="mx-3 mt-10 flex justify-between">
-                  <el-button type="danger" >Cancelar</el-button>
-                  <el-button type="success" >Confirmar</el-button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div  class=" absolute inset-0 z-40 bg-black opacity-25"></div>
-        </div>
-      </div> --> 
-
-
-      
-       <!-- <div class=" mb-10 h-5 w-full bg-gray-700">
-        <div class="text-center">
-          <span class=" font-sans text-xs font-bold text-white">Senha</span>
-        </div>
-        <div class=" mr-3 flex items-center justify-between">
-          <span class=" my-1 ml-3 block font-sans text-xs font-medium leading-5">********</span>
-          <el-button type="primary" icon="el-icon-edit-outline" circle size="mini" ></el-button>
-          <div  class="absolute inset-0 z-50 flex items-center justify-center overflow-y-auto overflow-x-hidden">
-            <div class=" relative m-auto">
-              <div class=" h-56 w-80 rounded bg-white">
-                <div class=" mt-5 text-center">
-                  <span class=" text-base font-bold">Editar Senha</span>
-                </div>
-                <el-form ref="modalPasswordForm">
-                  <div class=" mx-3 mt-3 mb-2">
-                    <el-form-item prop="inputOldPassword">
-                      <el-input placeholder="Senha Atual" show-password></el-input>
-                    </el-form-item>
-                  </div>
-                  <div class="mx-3">
-                    <el-form-item prop="inputNewPassword">
-                      <el-input  placeholder="Nova Senha" show-password></el-input>
-                    </el-form-item>
-                  </div>
-                </el-form>
-                  <div class="mx-3 mt-11 flex justify-between">
-                  <el-button type="danger" >Cancelar</el-button>
-                  <el-button type="success" >Confirmar</el-button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div  class=" absolute inset-0 z-40 bg-black opacity-25"></div>
-        </div>
-      </div> -->
-      
 
      </div>
    
-     <div class=" absolute left-0 bottom-0 h-14 w-full bg-gray-700">
+    <div class=" absolute left-0 bottom-0 h-14 w-full bg-gray-700">
       <div class=" mx-8 my-2 flex items-center justify-between">
         <div>
           <i class="el-icon-user mx-5 text-2xl text-white"></i>
@@ -248,8 +241,146 @@
           <span class=" flex text-xs text-white">Suporte</span>
         </div>
       </div> 
-    </div>
+    </div> 
   </div>  
 </template>
 
+<script>
 
+// import axios from 'axios';
+
+export default {
+  name: 'UserProfile',
+  layout: 'empty',
+  data() {
+    return {
+      user: {
+        name: '',
+        email: '',
+        password: '',
+        id: '',
+      },
+      modalPassword: {
+        birthDate: '',
+        address: '',
+        contact: '',
+        emergencyContact: '',
+        weight: '',
+        statute: '',
+        ethnicity: '',
+        gender: '',
+        healthPlan: '',
+        imc: '',
+        observation: '',
+      },
+      toggleModalPassword: false,
+
+
+      rulesModalPassword: {
+        birthDate: [
+          { required: true, message: 'Campo obrigatório', trigger: 'blur' },
+        ],
+        address: [
+          { required: true, message: 'Campo obrigatório', trigger: 'blur' },
+        ],
+        contact: [
+          { required: true, message: 'Campo obrigatório', trigger: 'blur' },
+        ],
+        emergencyContact: [
+          { required: true, message: 'Campo obrigatório', trigger: 'blur' },
+        ],
+        weight: [
+          { required: true, message: 'Campo obrigatório', trigger: 'blur' },
+        ],
+        stature: [
+          { required: true, message: 'Campo obrigatório', trigger: 'blur' },
+        ],
+        ethnicity: [
+          { required: true, message: 'Campo obrigatório', trigger: 'blur' },
+        ],
+        gender: [
+          { required: true, message: 'Campo obrigatório', trigger: 'blur' },
+        ],
+        healthPlan: [
+          { required: true, message: 'Campo obrigatório', trigger: 'blur' },
+        ],
+        imc: [
+          { required: true, message: 'Campo obrigatório', trigger: 'blur' },
+        ],
+        observation: [
+          { required: true, message: 'Campo obrigatório', trigger: 'blur' },
+        ],
+      },
+    };
+  },
+  // async created() {
+  //   this.getUser();
+  // },
+  // methods: {
+  //   async getUser() {
+  //     axios.defaults.headers.common.Authorization = `Bearer ${localStorage.getItem('token')}`;
+  //     const { data } = await axios.get(`http://localhost:3333/users/getMe/${localStorage.getItem('usr-id')}`);
+  //     this.user.name = data.name;
+  //     this.user.email = data.email;
+  //     this.user.id = data.id;
+  //     this.user.password = data.password;
+  //   },
+  //   async updateName() {
+  //     const newName = {
+  //       name: this.modalName.inputNewName,
+  //     };
+  //     await axios.patch(`http://localhost:3333/users/editMe/${this.user.id}`, newName);
+  //     this.user.name = this.modalName.inputNewName;
+  //   },
+  //   submitName() {
+  //     this.$refs.modalNameForm.validate((valid) => {
+  //       if (valid) {
+  //         this.updateName();
+  //       } else {
+  //         this.$notify.error({
+  //           title: 'Erro',
+  //           message: 'Nome ou senha inválidos',
+  //         });
+  //       }
+  //     });
+  //   },
+  //   async updateEmail() {
+  //     const newEmail = {
+  //       email: this.modalEmail.inputNewEmail,
+  //     };
+  //     await axios.patch(`http://localhost:3333/users/editMe/${this.user.id}`, newEmail);
+  //     this.user.email = this.modalEmail.inputNewEmail;
+  //   },
+  //   submitEmail() {
+  //     this.$refs.modalEmailForm.validate((valid) => {
+  //       if (valid && this.inputConfirmEmail === this.inputNewEmail) {
+  //         this.updateEmail();
+  //       } else {
+  //         this.$notify.error({
+  //           title: 'Erro',
+  //           message: 'Email ou senha inválidos',
+  //         });
+  //       }
+  //     });
+  //   },
+  //   async updatePassword() {
+  //     const newPassword = {
+  //       password: this.modalPassword.inputNewPassword,
+  //     };
+  //     await axios.patch(`http://localhost:3333/users/editMe/${this.user.id}`, newPassword);
+  //   },
+  //   submitPassword() {
+  //     this.$refs.modalPasswordForm.validate((valid) => {
+  //       if (valid) {
+  //         this.updatePassword();
+  //       } else {
+  //         this.$notify.error({
+  //           title: 'Erro',
+  //           message: 'Senha inválida',
+  //         });
+  //       }
+  //     });
+  //   },
+  // },
+};
+</script>
