e7eb108907d3735fd1229b2961d4447463fe804b
William Reticena <william.wsd43@gmail.com>
2022-11-10 16:39:25 -0300

    style: padronizando o formulário de equilíbrio

--- components/evaluationsForms/bodyCompositionForm/index.vue
+++ components/evaluationsForms/bodyCompositionForm/index.vue
@@ -281,7 +281,6 @@ export default {
   name: 'BodyCompositionForm',
   data() {
     return {
-      studentId: 'f03c0f33-f967-4bef-b425-284335ca2d6c',
       mockup: {
         sex: 'Homem',
         age: 70,
